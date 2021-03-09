a6c19747b0421435c0c8656db4cf0a918c2676fa
Alexander Davis <talexanderd99@gmail.com>
2021-03-08 20:13:08 -0600

    Implemented node selection. Set up code to update node position when dragged.

--- argumengine/src/ArgGraph.js
+++ argumengine/src/ArgGraph.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { Stage, Layer, Circle } from "react-konva";
+import { Stage, Layer, Circle, Text } from "react-konva";
 import Konva from "konva";
 
 const Node = (props) => {
-  const [x, setX] = React.useState();
-  const [y, setY] = React.useState();
   const [text, setText] = React.useState();
 
   return (
-    <Circle
+    <Text
+      text={"x: " + props.x + " y: " + props.y}
       x={props.x}
       y={props.y}
       draggable
-      radius={50}
-      fill={"white"}
-      stroke={"black"}
-      strokeWidth={4}
-      onMouseDown={() => props.toggleSelected(this)}
-    ></Circle>
+      onMouseDown={() => props.toggleSelected(props.id)}
+      onDragMove={(e) => {
+        var pos = e.currentTarget.getAbsolutePosition();
+        props.updateNodePos(props.id, pos.x, pos.y);
+      }}
+    ></Text>
   );
 };
 
@@ -26,17 +25,15 @@ const ArgGraph = () => {
   const [args, setArgs] = React.useState([]);
   const [conflicts, setConflicts] = React.useState([]);
 
-  const [selected, setSelected] = React.useState([]);
-
-  const toggleSelected = (item) => {
-    let index = selected.indexOf(item);
-    let inArray = index !== -1;
-    if (inArray) setSelected(selected.filter((x, i) => i !== index));
-    else setSelected(selected.concat(item));
-    console.log("filter: " + selected.filter((x, i) => i !== index));
-    console.log("concat: " + selected.concat(item));
-    console.log("item: " + item);
-    console.log("selected: " + selected);
+  const toggleSelected = (selectedIndex) => {
+    setNodes(
+      nodes.map((node, index) => {
+        if (index == selectedIndex) {
+          node.selected = !node.selected;
+          return node;
+        } else return node;
+      })
+    );
   };
 
   const [mousePos, setMousePos] = React.useState();
@@ -47,14 +44,29 @@ const ArgGraph = () => {
 
   const spawnNode = () => {
     setNodes(
-      nodes.concat(
-        <Node
-          x={mousePos.x}
-          y={mousePos.y}
-          toggleSelected={toggleSelected}
-        ></Node>
-      )
+      nodes.concat({
+        x: mousePos.x,
+        y: mousePos.y,
+        selected: false,
+      })
     );
+  };
+
+  const updateNodePos = (selectedIndex, x, y) => {
+    setNodes(
+      nodes.map((node, index) => {
+        if (index == selectedIndex) {
+          node.x = x;
+          node.y = y;
+          return node;
+        } else return node;
+      })
+    );
+    console.log(nodes);
+  };
+
+  const deleteNode = (node) => {
+    setNodes(nodes.filter((x, i) => i !== nodes.indexOf(node)));
     console.log(nodes);
   };
 
@@ -65,7 +77,20 @@ const ArgGraph = () => {
       onMouseMove={trackMouse}
       onDblClick={spawnNode}
     >
-      <Layer>{nodes}</Layer>
+      <Layer>
+        {nodes.map((node, i) => {
+          return (
+            <Node
+              key={i}
+              id={i}
+              x={node.x}
+              y={node.y}
+              toggleSelected={toggleSelected}
+              updateNodePos={updateNodePos}
+            ></Node>
+          );
+        })}
+      </Layer>
     </Stage>
   );
 };
